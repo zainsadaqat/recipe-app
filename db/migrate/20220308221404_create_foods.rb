6dde61f3b233364dd14d5651ce4a3fc2d1cc4361
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-09 03:54:06 +0500

    Create respective migrations

---
+++ db/migrate/20220308221404_create_foods.rb
@@ -0,0 +1,11 @@
+class CreateFoods < ActiveRecord::Migration[7.0]
+  def change
+    create_table :foods do |t|
+      t.belongs_to :user, foreign_key: true, index: true
+      t.integer :measurement_unit
+      t.integer :price
+      
+      t.timestamps
+    end
+  end
+end
