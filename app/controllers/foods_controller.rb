a6095b3a56b6c260e3a29ba9603979adb8b2fe7c
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-09 04:09:29 +0500

    Implement all crud actions for food ctr

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,11 +1,41 @@
 class FoodsController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.all
+  end
 
-  def show; end
+  def show
+    @food = Food.find(params[:id])
+  end
 
-  def new; end
+  def new
+    @new_food = Food.new
+  end
 
-  def create; end
+  def create
+    @new_food = Food.new(
+      user: current_user,
+      name: params[:food][:name],
+      measurement_unit: params[:food][:measurement_unit],
+      price: params[:food][:price]
+    )
 
-  def destroy; end
+    if @new_food.save
+      redirect_to foods_path, notice: 'Ingridiant successfully added'
+    else
+      render :new, alert: 'Error: Ingridiant could not be created'
+    end
+  end
+
+  def destroy
+    Food.find_by(id: params[:id]).destroy
+    respond_to do |format|
+      format.html { redirect_to root_path, notice: 'Ingridiant was successfully removed' }
+    end
+  end
+
+  private
+
+  def food_params
+    params.required(:food).permit(:name, :measurement_unit, :price)
+  end
 end
