256da5891931dc6fac6af0a429e49fd858a5d599
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-08 22:40:35 +0500

    Set up devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Temp home page
+  root to: "users#index"
 
   resources :users, only: %i[index show]
 
