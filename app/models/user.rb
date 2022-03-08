c65f40073235144f4b1601b2900707473f3aa54e
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-09 02:57:35 +0500

    Add name input for registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: { message: 'This field can not be blank' }
 end
