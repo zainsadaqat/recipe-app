36594a4961f1a309f61844e169510b7f61eb9399
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-10 04:43:10 +0500

    Add foodRecipe ctr

--- app/controllers/foods_recipes_controller.rb
+++ app/controllers/foods_recipes_controller.rb
@@ -1,8 +1,29 @@
 class FoodsRecipesController < ApplicationController
-  def new; end
+  def new
+    @food_recipe = FoodRecipe.new
+  end
 
-  def create; end
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @food = Food.find_by(id: params[:food_id])
+    @new_food_recipe = FoodRecipe.new(
+      food: @food,
+      recipe: @recipe,
+      quantity: params[:quantity]
+    )
 
-  def destroy; end
-  
+    if @new_food_recipe.save
+      redirect_to recipe_path({ id: @recipe.id }), notice: 'Ingridient seccessfully added'
+    else
+      render :new, notice: 'Somethig went wrong'
+    end
+  end
+
+  def destroy
+    FoodRecipe.find_by(id: params[:id]).destroy
+
+    respond_to do |format|
+      format.html { redirect_to recipe_path({ id: @recipe.id }), notice: 'Ingridient successfully removed' }
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, presence: { message: 'This field can not be blank' }
-  
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   has_many :recipes
   has_many :foods
-
 end
