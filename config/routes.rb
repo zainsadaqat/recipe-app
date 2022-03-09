87fabbb0fd1e39186faec7eb62303e1e1134fd64
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-10 02:28:42 +0500

    Create routes for recipes and public_recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
+  resources :recipes
+  get '/public_recipes', to: "recipes#public_recipes"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Temp home page
   root to: "users#index"
-
   resources :users, only: %i[index show]
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
