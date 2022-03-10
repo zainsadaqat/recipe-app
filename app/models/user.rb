2b8e945d2cbe572dd0502ff66c167b84218c299f
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-11 02:37:54 +0500

    Refactor user model to integrate with food recipe

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, presence: { message: 'This field can not be blank' }
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
-  has_many :recipes
-  has_many :foods
+  has_many :recipes, dependent: :delete_all
+  has_many :foods, dependent: :delete_all
 end
