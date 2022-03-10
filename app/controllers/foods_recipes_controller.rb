7341d28d4e4e484360eb10347168f0fc24746d80
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-03-11 02:38:32 +0500

    Refactor food recipe cntr

--- app/controllers/foods_recipes_controller.rb
+++ app/controllers/foods_recipes_controller.rb
@@ -1,29 +1,27 @@
 class FoodsRecipesController < ApplicationController
   def new
-    @food_recipe = FoodRecipe.new
+    @food_recipe = FoodsRecipe.new
   end
 
   def create
-    @recipe = Recipe.find(params[:recipe_id])
-    @food = Food.find_by(id: params[:food_id])
-    @new_food_recipe = FoodRecipe.new(
-      food: @food,
-      recipe: @recipe,
+    @new_food_recipe = FoodsRecipe.new(
+      food_id: params[:food_id],
+      recipe_id: params[:recipe_id],
       quantity: params[:quantity]
     )
 
     if @new_food_recipe.save
-      redirect_to recipe_path({ id: @recipe.id }), notice: 'Ingridient seccessfully added'
+      redirect_to recipe_path({ id: params[:recipe_id] }), notice: 'Ingridient seccessfully added'
     else
       render :new, notice: 'Somethig went wrong'
     end
   end
 
   def destroy
-    FoodRecipe.find_by(id: params[:id]).destroy
+    FoodsRecipe.find_by(id: params[:id]).destroy
 
     respond_to do |format|
-      format.html { redirect_to recipe_path({ id: @recipe.id }), notice: 'Ingridient successfully removed' }
+      format.html { redirect_to recipe_path({ id: params[:recipe_id] }), notice: 'Ingridient successfully removed' }
     end
   end
 end
