522cb46c0272492eaae6552e19077d7639fea4c5
Zain Sadaqat <zain.sadaqet@gmail.com>
2022-03-15 02:43:43 +0500

    Create rspec:model

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,17 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'Recipe' do
-  context 'context' do
-    before do
-    user = User.create(email: 'john@example.com', password: '123123', password_confirmation: '123123');
-    book = Recipe.create!(name: 'BBQ', preparation_time: '3Hrs', cooking_time: '2Hrs', description: 'Lorem ipsum is a paragraph', user_id: user.id);
-    visit root_path
-    click_link('New book')
-    end
-
-    it 'page to create a new book' do
-    expect(page).to have_field('title')
-    expect(page).to have_field('author')
+RSpec.describe Recipe, type: :model do
+  context "Recipes" do
+    it "Show Recipes" do
+      expect(page).to have_text("recipes")
     end
   end
 end
